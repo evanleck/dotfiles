f3a2a765804b62fdd828224a78b837953855d03b
Evan Lecklider <evan@lecklider.com>
2021-09-01 15:15:02 -0700

    Disable commit confirmation

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -174,6 +174,7 @@ require('packer').startup({
       },
       config = function()
         require('neogit').setup {
+          disable_commit_confirmation = true,
           integrations = {
             diffview = true
           }
