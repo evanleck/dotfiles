2b1f62537115c49a4261c9dae9b6ec49a48867fa
Evan Lecklider <evan@lecklider.com>
2021-10-27 14:01:25 -0700

    The return of fish shell

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,91 +1,35 @@
-# Load ASDF
-#   Note that is equivalent to, but faster than, running (brew --prefix asdf).
-source /usr/local/opt/asdf/asdf.fish
+# Pull in asdf.
+source /usr/local/opt/asdf/libexec/asdf.fish
 
-# Load Autojump
-source /usr/local/share/autojump/autojump.fish
+# Add Doom to our PATH.
+fish_add_path ~/.emacs.d/bin
 
-# FZF configuration.
-set -x FZF_DEFAULT_COMMAND 'rg --files'
-set -x FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
-set -x FZF_ALT_C_COMMAND "$FZF_DEFAULT_COMMAND"
+# Add our own bin directory to the PATH.
+fish_add_path ~/.bin
 
-# Tell ripgrep where to look for configuration.
-set -x RIPGREP_CONFIG_PATH "$HOME/.config/ripgrep/config"
-
-# Tell Homebrew to cleanup.
-set -x HOMEBREW_INSTALL_CLEANUP
-set -x HOMEBREW_NO_GITHUB_API
-
-set -x EDITOR 'mvim'
-set -x VIMHOME "$HOME/.vim"
+# Environment variables.
+set -x RIPGREP_CONFIG_PATH ~/.config/ripgrep/config
+set -x EDITOR nvim
 
 # Aliases.
 alias be 'bundle exec'
-alias brewup 'brew update; brew upgrade; brew cask upgrade'
 alias c clear
-alias cat bat # OMG where has this been all my life?!
-alias ls exa # Double OMG, just like above.
+alias cat bat
 alias g git
+alias ls exa
+
+# Modify Dracula's colors slightly.
+#   See conf.d/dracula.fish for the color definitions.
+#   See https://github.com/fish-shell/fish-shell/blob/master/doc_src/interactive.rst#syntax-highlighting-variables for variable names.
+set -g fish_color_command fish_color_normal --bold
+set -g fish_color_error ff5555 --bold # red
+set -g fish_color_param fish_color_normal
+
+# Modify hydro's colors.
+#   https://github.com/jorgebucaran/hydro#colors
+set -g hydro_color_pwd bd93f9 --bold # purple
+set -g hydro_color_git ff79c6 --bold # pink
+set -g hydro_color_prompt 50fa7b --bold # green
 
 # Quiet down the greeting.
 set fish_greeting ''
-
-# name: Robbyrussell
-# author: Bruno Ferreira Pinto, Pawel Zubrycki
-function fish_prompt
-  if not set -q -g __fish_robbyrussell_functions_defined
-    set -g __fish_robbyrussell_functions_defined
-    function _git_branch_name
-      echo (git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
-    end
-
-    function _is_git_dirty
-      echo (git status -s --ignore-submodules=dirty ^/dev/null)
-    end
-
-    function _is_git_repo
-      git status -s >/dev/null ^/dev/null
-    end
-
-    function _repo_branch_name
-      eval "_$argv[1]_branch_name"
-    end
-
-    function _is_repo_dirty
-      eval "_is_$argv[1]_dirty"
-    end
-
-    function _repo_type
-      if _is_git_repo
-        echo 'git'
-      end
-    end
-  end
-
-  set -l cyan (set_color -o cyan)
-  set -l yellow (set_color -o yellow)
-  set -l red (set_color -o red)
-  set -l blue (set_color -o blue)
-  set -l normal (set_color normal)
-
-  set -l arrow "$red➜ "
-  if [ $USER = 'root' ]
-    set arrow "$red# "
-  end
-
-  set -l cwd $cyan(basename (prompt_pwd))
-
-  set -l repo_type (_repo_type)
-  if [ $repo_type ]
-    set -l repo_branch $red(_repo_branch_name $repo_type)
-    set repo_info "$blue $repo_type:($repo_branch$blue)"
-
-    if [ (_is_repo_dirty $repo_type) ]
-      set -l dirty "$yellow ✗"
-      set repo_info "$repo_info$dirty"
-    end
-  end
-
-  echo -n -s $arrow ' '$cwd $repo_info $normal ' '
-end
