69d26676308f8150b5eb438913eab6183a1dc474
Evan Lecklider <evan@lecklider.com>
2022-01-27 11:15:39 -0800

    Add a space around hydro's git dirty symbol

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -32,6 +32,7 @@ set -g fish_color_param fish_color_normal
 set -g hydro_color_pwd bd93f9  # purple
 set -g hydro_color_git 6272a4  # comment
 set -g hydro_color_prompt f1fa8c  # yellow
+set -g hydro_symbol_git_dirty ' •'
 
 # Quiet down the greeting.
 set fish_greeting ''
