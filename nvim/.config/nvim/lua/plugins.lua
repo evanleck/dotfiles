d01f2a94df2a37076aa8b219201167f962ca8cca
Evan Lecklider <evan@lecklider.com>
2022-04-06 16:33:33 -0700

    Fix completion in neovim

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -23,48 +23,53 @@ require('packer').startup({
 				'williamboman/nvim-lsp-installer'
 			},
 			config = function()
-				-- Taken directly from
-				-- https://github.com/neovim/nvim-lspconfig#keybindings-and-completion
+				-- Taken directly from https://github.com/neovim/nvim-lspconfig#suggested-configuration
+
+				-- Mappings.
+				-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+				local opts = { noremap = true, silent = true }
+				vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+				vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+				vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+				vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+
 				-- Use an on_attach function to only map the following keys
 				-- after the language server attaches to the current buffer
 				local on_attach = function(client, bufnr)
-					local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-					local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-					--Enable completion triggered by <c-x><c-o>
-					buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+					-- Enable completion triggered by <c-x><c-o>
+					vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
 					-- Mappings.
-					local opts = { noremap = true, silent = true }
-
 					-- See `:help vim.lsp.*` for documentation on any of the below functions
-					buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-					buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-					buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-					buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-					buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-					buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-					buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-					buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-					buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-					buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-					buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-					buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-					buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-					buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-					buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-					buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-					buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+					vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+					vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+					vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+					vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+					vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+					vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+					vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+					vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+					vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+					vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+					vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+					vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 				end
 
 				local capabilities = vim.lsp.protocol.make_client_capabilities()
 				capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-				local opts = { capabilities = capabilities, on_attach = on_attach }
-				local lsp_installer = require('nvim-lsp-installer')
+
+				local opts = {
+					capabilities = capabilities,
+					on_attach = on_attach,
+					flags = {
+						-- This will be the default in neovim 0.7+
+						debounce_text_changes = 150,
+					}
+				}
 
 				-- Register a handler that will be called for all installed servers.
 				-- Alternatively, you may also register handlers on specific server instances instead (see example below).
-				lsp_installer.on_server_ready(function(server)
+				require('nvim-lsp-installer').on_server_ready(function(server)
 					-- This setup() function is exactly the same as lspconfig's setup function.
 					-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 					server:setup(opts)
@@ -77,29 +82,41 @@ require('packer').startup({
 			'hrsh7th/nvim-cmp',
 			requires = {
 				'hrsh7th/cmp-buffer',
-				'hrsh7th/cmp-nvim-lsp',
-				'hrsh7th/vim-vsnip'
+				'hrsh7th/cmp-nvim-lsp'
 			},
 			config = function()
 				local cmp = require('cmp')
 
 				cmp.setup({
-					snippet = {
-						expand = function(args)
-							vim.fn["vsnip#anonymous"](args.body)
-						end,
-					},
 					mapping = {
-						['<Tab>'] = cmp.mapping.select_next_item(),
-						['<S-Tab>'] = cmp.mapping.select_prev_item(),
-						['<CR>'] = cmp.mapping.confirm({
+						['<C-p>'] = cmp.mapping.select_prev_item(),
+						['<C-n>'] = cmp.mapping.select_next_item(),
+						['<C-d>'] = cmp.mapping.scroll_docs(-4),
+						['<C-f>'] = cmp.mapping.scroll_docs(4),
+						['<C-Space>'] = cmp.mapping.complete(),
+						['<C-e>'] = cmp.mapping.close(),
+						['<CR>'] = cmp.mapping.confirm {
 							behavior = cmp.ConfirmBehavior.Replace,
 							select = true,
-						})
+						},
+						['<Tab>'] = function(fallback)
+							if cmp.visible() then
+								cmp.select_next_item()
+							else
+								fallback()
+							end
+						end,
+						['<S-Tab>'] = function(fallback)
+							if cmp.visible() then
+								cmp.select_prev_item()
+							else
+								fallback()
+							end
+						end,
 					},
 					sources = {
-						{ name = 'nvim_lsp' }
-					}
+						{ name = 'nvim_lsp' },
+					},
 				})
 			end
 		}
