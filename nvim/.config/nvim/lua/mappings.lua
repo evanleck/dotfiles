874361c33025c3ae23ed738222fde5e0cc1c69cc
Evan Lecklider <evan@lecklider.com>
2022-04-14 11:52:38 -0700

    Remove unused local variables in mappings.lua

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -1,8 +1,3 @@
-local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
-local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
-local g = vim.g      -- a table to access global variables
-local opt = vim.opt  -- to set options
-
 local function map(mode, lhs, rhs, opts)
 	local options = { noremap = true }
 	if opts then options = vim.tbl_extend('force', options, opts) end
@@ -15,7 +10,7 @@ map('n', 'k', 'gk', { noremap = false })
 
 -- Doom Emacs kinda set the stage for this one...
 map('n', '<Space>', '', { noremap = false })
-g.mapleader = ' '
+vim.g.mapleader = ' '
 
 -- Copy to clipboard in normal, visual, select and operator modes
 map('', '<Leader>y', '"+y')
