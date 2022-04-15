26a8c07f956bb0f5a2230b0897492ff1d141fbb9
Evan Lecklider <evan@lecklider.com>
2022-04-15 13:20:50 -0700

    Add fzf colors

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -189,7 +189,22 @@ require('packer').startup({
 							default = 'bat_native',
 							hidden = 'hidden'
 						}
-					}
+					},
+					fzf_colors = {
+						["fg"]          = { "fg", "CursorLine" },
+						["bg"]          = { "bg", "Normal" },
+						["hl"]          = { "fg", "Comment" },
+						["fg+"]         = { "fg", "Normal" },
+						["bg+"]         = { "bg", "CursorLine" },
+						["hl+"]         = { "fg", "Statement" },
+						["info"]        = { "fg", "PreProc" },
+						["prompt"]      = { "fg", "Conditional" },
+						["pointer"]     = { "fg", "Exception" },
+						["marker"]      = { "fg", "Keyword" },
+						["spinner"]     = { "fg", "Label" },
+						["header"]      = { "fg", "Comment" },
+						["gutter"]      = { "bg", "Normal" },
+					},
 				})
 			end
 		}
