7fce173833b00cb67f600e5cda5ecdddfa37b49f
Evan Lecklider <evan@lecklider.com>
2022-09-08 13:31:43 -0700

    Fix config.fish test for Intel Homebrew

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,7 @@
 # Load in Homebrew
 if test -f /opt/homebrew/bin/brew
 	eval (/opt/homebrew/bin/brew shellenv)
-else if -f /usr/local/bin/brew
+else if test -f /usr/local/bin/brew
 	eval (/usr/local/bin/brew shellenv)
 end
 
