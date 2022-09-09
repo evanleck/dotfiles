d3c45d68a3b238c82014143fd2f4c32f11c8e97e
Evan Lecklider <evan@lecklider.com>
2022-09-09 14:22:47 -0700

    Switch to hunspell and disable grammar checker

--- doom/.doom.d/init.el
+++ doom/.doom.d/init.el
@@ -83,8 +83,8 @@
 
 	:checkers
 	syntax              ; tasing you for every semicolon you forget
-	(spell +flyspell) ; tasing you for misspelling mispelling
-	grammar           ; tasing grammar mistake every you make
+	(spell +flyspell +hunspell) ; tasing you for misspelling mispelling
+	;;grammar           ; tasing grammar mistake every you make
 
 	:tools
 	;;ansible
