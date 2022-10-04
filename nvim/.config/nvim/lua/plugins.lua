ab7c25e87f0db2ff21e704d6cf2e9811d70adced
Evan Lecklider <evan@lecklider.com>
2022-10-04 09:57:12 -0700

    Ignore phpdoc tree-sitter config

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -47,6 +47,7 @@ require('packer').startup({
 			config = function()
 				require('nvim-treesitter.configs').setup {
 					ensure_installed = 'all',
+					ignore_install = { 'phpdoc' },
 					highlight = {
 						enable = true
 					},
