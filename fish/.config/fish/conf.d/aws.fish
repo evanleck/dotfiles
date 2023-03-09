fb17c0cd659dc25ee3cf90327355b2ae6e954f1f
Evan Lecklider <evan@lecklider.com>
2023-03-09 10:55:40 -0800

    Add AWS CLI completions

---
+++ fish/.config/fish/conf.d/aws.fish
@@ -0,0 +1,7 @@
+# https://github.com/oh-my-fish/plugin-aws/blob/master/conf.d/aws.fish
+set -e aws_profile
+
+if not set -q aws_completer_path
+	set -g aws_completer_path (type -P aws_completer 2> /dev/null)
+		or echo "aws: unable to find aws_completer, completions unavaliable"
+end
