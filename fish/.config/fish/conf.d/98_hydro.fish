2e34ffd10b0ab456786b4c964527f4e29791945c
Evan Lecklider <evan@lecklider.com>
2023-08-18 14:32:21 -0700

    Tweak hydro colors using fish color variables

--- fish/.config/fish/conf.d/98_hydro.fish
+++ fish/.config/fish/conf.d/98_hydro.fish
@@ -1,6 +1,5 @@
-# Modify hydro's colors.
-#   https://github.com/jorgebucaran/hydro#colors
-set -g hydro_color_pwd bd93f9  # purple
-set -g hydro_color_git 6272a4  # comment
-set -g hydro_color_prompt f1fa8c  # yellow
 set -g hydro_symbol_git_dirty ' •'
+
+# Modify hydro's colors based on the theme.
+#   https://github.com/jorgebucaran/hydro#colors
+set -g hydro_color_pwd $fish_color_cwd
