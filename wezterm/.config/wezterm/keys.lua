97b04165e8f5c6986fb6b287befcb3a770cc2923
Evan Lecklider <evan@lecklider.com>
2024-08-13 14:10:15 -0700

    Add initial WezTerm config
    Damn you, https://alexplescan.com/posts/2024/08/10/wezterm/

---
+++ wezterm/.config/wezterm/keys.lua
@@ -0,0 +1,40 @@
+local wezterm = require 'wezterm'
+
+return {
+  {
+    key = 'd',
+    mods = 'CMD',
+    action = wezterm.action.SplitPane {
+      direction = 'Right',
+      size = { Percent = 50 },
+    },
+  },
+  {
+    key = 'd',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.SplitPane {
+      direction = 'Down',
+      size = { Percent = 50 },
+    },
+  },
+  {
+    key = 'f',
+    mods = 'CMD|CTRL',
+    action = wezterm.action.ToggleFullScreen,
+  },
+  {
+    key = 'w',
+    mods = 'CMD',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+  {
+    key = '[',
+    mods = 'CMD',
+    action = wezterm.action.ActivatePaneDirection('Prev')
+  },
+  {
+    key = ']',
+    mods = 'CMD',
+    action = wezterm.action.ActivatePaneDirection('Next')
+  }
+}
