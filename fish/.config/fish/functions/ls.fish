c1b2f94de0662da281c0b26b0321cb2c5ca5e4bf
Evan Lecklider <evan@lecklider.com>
2026-01-29 14:34:33 -0800

    Add colors and config for lsd if needed

--- fish/.config/fish/functions/ls.fish
+++ fish/.config/fish/functions/ls.fish
@@ -2,4 +2,8 @@ if type -q eza
     function ls --wraps eza
         command eza --header --long --git --all $argv
     end
+else if type -q lsd
+    function ls --wraps lsd
+        command lsd --long $argv
+    end
 end
