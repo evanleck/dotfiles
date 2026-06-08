283e27001c695f76f625c03b4ec501c1467fb703
Evan Lecklider <evan@lecklider.com>
2026-06-08 10:05:43 -0700

    Add fish_remove_path function

---
+++ fish/.config/fish/functions/fish_remove_path.fish
@@ -0,0 +1,9 @@
+function fish_remove_path
+    if set -l index (contains -i $argv[1] $fish_user_paths)
+        set --erase --universal fish_user_paths[$index]
+        echo "Updated fish_user_paths: $fish_user_paths"
+    else
+        echo "$argv[1] not found in fish_user_paths: $fish_user_paths"
+    end
+end
+
